066a788cb39025ce0c67da80fd28892608593984
Sukhrob Abdullaev <sukhrobjonabdullaev3365@gmail.com>
2023-09-24 16:26:29 +0500

    added projects

---
+++ src/components/ProjectImages.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export const ImageWithDescription = ({
+  imageUrl,
+  description,
+  preview,
+  title,
+}) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div>
+      <h4 className="text-left fade-opacity md:my-6 mb-6 font-medium md:text-[25px] title fade-opacityLeft">
+        {title}
+      </h4>
+      <div
+        className="relative group cursor-pointer"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <img
+          src={imageUrl}
+          alt={imageUrl}
+          loading="lazy"
+          className="w-[600px] fade-opacityNext md:w-full"
+        />
+        <div
+          className={`${
+            hovered
+              ? "opacity-100 translate-y-0 h-1/2"
+              : "opacity-0 translate-y-4"
+          } transition-all duration-300 ease-in-out absolute bottom-0 left-0 right-0 bg-[rgba(0,0,0,0.6)]  p-2 rounded-t fade-opacityNext`}
+        >
+          <div>
+            <p>{description}</p>
+            <a href={preview}>Demo</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,28 +1,53 @@
-import React from "react";
+import { ImageWithDescription } from "../components/ProjectImages";
 
 const Projects = () => {
+  const images = [
+    {
+      id: 1,
+      title: "Nayzak E-Commerce @Upwork",
+      imageUrl: "/projects/Nayzak_E-Commerce.png",
+      description: "Description for Image 1",
+      preview: "https://nayzaki.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Code Crafters @CXC",
+      imageUrl: "/projects/CodeCrafters.png",
+      description: "Description for Image 2",
+      preview: "https://codecrafts-alisher1403.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "QR Code Generator - Personal Projects",
+      imageUrl: "/projects/QR_coder_generator.png",
+      description: "Description for Image 3",
+      preview: "https://qr-code-generator-vite.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "Weather App - Personal Projects",
+      imageUrl: "/projects/Nayzak_E-Commerce.png",
+      description: "Description for Image 4",
+      preview: "",
+    },
+  ];
+
   return (
     <div className="py-4 text-center w-full mx-auto max-w-[350px] md:max-w-[1336px] md:mx-auto">
-      <h2 className="md:text-[40px] text-lg font-mono font-bold mb-4">
+      <h2 className="md:text-[40px] text-lg font-mono font-bold mb-4 fade-opacityRight">
         Some of the projects <br /> that I have worked on
       </h2>
       <div className="projects shadow-md md:p-8 relative">
-        <h4 className="text-left fade-opacity mb-6 font-medium md:text-[25px] title">
-          Personal projects
-        </h4>
-        <div className="demos md:flex md:justify-between flex md:flex-row md:items-center flex-col md:gap-0 gap-10">
-          <img
-            src="/projects/Nayzak_E-Commerce.png"
-            alt="Nayzaki_demo"
-            loading="lazy"
-            className="md:w-[600px] fade-opacityNext"
-          />
-          <img
-            src="/projects/CodeCrafters.png"
-            alt="CodeCrafters_demo"
-            loading="lazy"
-            className="md:w-[600px] fade-opacityNext"
-          />
+        <div className="demos md:flex md:justify-between flex md:items-center flex-col md:gap-0 gap-6 text-white">
+          {images.map((image) => (
+            <ImageWithDescription
+              key={image.id}
+              imageUrl={image.imageUrl}
+              description={image.description}
+              preview={image.preview}
+              title={image.title}
+            />
+          ))}
         </div>
       </div>
     </div>
